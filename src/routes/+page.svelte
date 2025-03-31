8750b64baf4605a9aa92fb2f7a7553fa752a839a
sean boose <seanboose@gmail.com>
2025-03-31 16:00:11 -0700

    add sample image from vercel blob

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<img src="https://wunqjzmni9wpumjc.public.blob.vercel-storage.com/test/pedestrian-2-OslaBYEiCaF6lGR7ftY0BNUU13R380.png" alt="sample"/>
